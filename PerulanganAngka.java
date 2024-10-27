1fee25e680cdc3b612953867508dffa1454f6e54
nazwa121 <nazwahafizah2305@gmail.com>
2024-10-27 14:58:53 +0700

    first commit

---
+++ PerulanganAngka.java
@@ -0,0 +1,13 @@
+public class PerulanganAngka {
+    public static void main(String[] args) {
+        int i,j;
+
+        for (i = 1; i<=5;i++) {
+            System.out.println("");
+            for (j = 1; j<=i; j++){
+                System.out.println(j+" "); 
+            }
+
+        }
+    }
+}
